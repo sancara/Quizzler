5d33c2e66423739e0fed9431673f2ce6a470ac83
Sancara <santiagocaraballo@gmail.com>
2022-02-17 16:46:16 -0300

    refactoring to 2 games play

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -10,19 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //True or False
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
-   
+    
+    //multipleChoice Game
+    @IBOutlet weak var multipleChoiceScoreLabel: UILabel!
+    @IBOutlet weak var multipleChoiceQuestionLabel: UILabel!
+    @IBOutlet weak var multipleChoiceProgressBar: UIProgressView!
+    @IBOutlet weak var firstChoiceButton: UIButton!
+    @IBOutlet weak var secondChoiceButton: UIButton!
+    @IBOutlet weak var thirdChoiceButton: UIButton!
+    
     var quizBrain = QuizBrain()
+    var multipleQuiz = MultipleQuestionBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Multiple Choice
+//        multipleChoiceProgressBar.progress = 0.0
+//        updateMUI()
+        
+        //True or False
         progressBar.progress = 0.0
         updateUI()
+        
     }
 
     @IBAction func answerButtonPress(_ sender: UIButton) {
@@ -49,6 +65,28 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func multipleAnswerChoice(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        let userGotItWright = multipleQuiz.checkAnswer(userAnswer)
+        
+        if userGotItWright {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        multipleQuiz.nextQuestion()
+        
+        let seconds = 0.2
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            // Put your code which should be executed with a delay here
+            self.updateMUI()
+        }
+    }
+    
+    
+    
     func updateUI() {
         questionLabel.text = quizBrain.getQuestionText()
         progressBar.progress = quizBrain.getProgress()
@@ -57,5 +95,13 @@ class ViewController: UIViewController {
         falseButton.backgroundColor = UIColor.clear
     }
     
+    func updateMUI() {
+        multipleChoiceQuestionLabel.text = multipleQuiz.getQuestionText()
+        multipleChoiceProgressBar.progress = multipleQuiz.getProgress()
+        multipleChoiceScoreLabel.text = "Score: \(multipleQuiz.getScore())"
+        firstChoiceButton.backgroundColor = UIColor.clear
+        secondChoiceButton.backgroundColor = UIColor.clear
+        thirdChoiceButton.backgroundColor = UIColor.clear
+    }
 }
 
--- Quizzler-iOS13/Model/QuizBrain.swift
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -61,53 +61,5 @@ struct QuizBrain {
 
 }
 
-struct MultipleQuestionBrain {
-    let multipleQuiz = [
-        MultipleQuestion(q: "Which is the largest organ in the human body?", a: ["Heart", "Skin", "Large Intestine"], correct: "Skin"),
-        MultipleQuestion(q: "Five dollars is worth how many nickels?", a: ["25", "50", "100"], correct: "100"),
-        MultipleQuestion(q: "What do the letters in the GMT time zone stand for?", a: ["Global Meridian Time", "Greenwich Mean Time", "General Median Time"], correct: "Greenwich Mean Time"),
-        MultipleQuestion(q: "What is the French word for 'hat'?", a: ["Chapeau", "Écharpe", "Bonnet"], correct: "Chapeau"),
-        MultipleQuestion(q: "In past times, what would a gentleman keep in his fob pocket?", a: ["Notebook", "Handkerchief", "Watch"], correct: "Watch"),
-        MultipleQuestion(q: "How would one say goodbye in Spanish?", a: ["Au Revoir", "Adiós", "Salir"], correct: "Adiós"),
-        MultipleQuestion(q: "Which of these colours is NOT featured in the logo for Google?", a: ["Green", "Orange", "Blue"], correct: "Orange"),
-        MultipleQuestion(q: "What alcoholic drink is made from molasses?", a: ["Rum", "Whisky", "Gin"], correct: "Rum"),
-        MultipleQuestion(q: "What type of animal was Harambe?", a: ["Panda", "Gorilla", "Crocodile"], correct: "Gorilla"),
-        MultipleQuestion(q: "Where is Tasmania located?", a: ["Indonesia", "Australia", "Scotland"], correct: "Australia")
-    ]
 
-    var questionNumber = 0
-    var score = 0
-    
-    mutating func checkAnswer(_ userAnswer: String) -> Bool {
-         if userAnswer == multipleQuiz[questionNumber].correctAnswer {
-             self.score += 1
-             return true
-         } else {
-            return false
-         }
-     }
-    
-    func getQuestionText() -> String {
-        return multipleQuiz[questionNumber].text
-    }
-    
-    func getProgress() -> Float {
-        return Float(questionNumber + 1) / Float(multipleQuiz.count)
-    }
-    
-    mutating func nextQuestion() {
-        if questionNumber < multipleQuiz.count - 1 {
-            self.questionNumber += 1
-        } else {
-            self.questionNumber = 0
-            self.score = 0
-        }
-    }
-    
-    func getScore() -> Int {
-        return score
-    }
-    
-    
-}
 
